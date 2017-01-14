e941839fc1940150017d9c1bf45eb1d6a603ff33
Jean-Étienne <git@jeanetienne.net>
2017-01-14 17:07:24 +1100

    Basic Xcode setup

---
+++ EmojiSerializer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EmojiSerializer
+//
+//  Created by Jean-Étienne Parrot on 14/1/17.
+//  Copyright © 2017 Jean-Étienne. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
